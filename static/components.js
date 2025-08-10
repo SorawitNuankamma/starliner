43eb51f1a375f8e4f5609b7876dcaa0f94c28b19
unknown <sorawit.nuankamma@gmail.com>
2025-08-10 12:23:32 +0700

    add feature

--- static/components.js
+++ static/components.js
@@ -1,68 +1,68 @@
-a = document.createElement('a')
+a = document.createElement("a");
 
 class Component {
-  el = null
-  constructor(element){
-    this.el = element
+  el = null;
+  constructor(element) {
+    this.el = element;
   }
-  class(className){
-    this.el.classList.add(className)
-    return this
+  class(className) {
+    this.el.classList.add(className);
+    return this;
   }
-  child(...elements){
+  child(...elements) {
     elements.forEach((el) => {
-      this.el.appendChild(el)
-    })
-    return this
+      this.el.appendChild(el);
+    });
+    return this;
   }
-  type(type){
-    this.el.type = type
-    return this
+  type(type) {
+    this.el.type = type;
+    return this;
   }
-  name(name){
-    this.el.name = name
-    return this
+  name(name) {
+    this.el.name = name;
+    return this;
   }
-  value(value){
-    this.el.value = value
-    return this
+  value(value) {
+    this.el.value = value;
+    return this;
   }
-  content(content){
-    this.el.innerHTML = content
-    return this
+  content(content) {
+    this.el.innerHTML = content;
+    return this;
   }
-  get(){
-    return this.el
+  get() {
+    return this.el;
   }
 }
 
 class CF {
-  static create(name){
-    return new Component(document.createElement(name))
+  static create(name) {
+    return new Component(document.createElement(name));
   }
-  static createInput(name, type, value, className){
-    const el = document.createElement('input')
-    el.name = name
-    el.type = type
-    el.value = value
-    el.classList.add(className)
-    return new Component(el)
+  static createInput(name, type, value, className) {
+    const el = document.createElement("input");
+    el.name = name;
+    el.type = type;
+    el.value = value;
+    el.classList.add(className);
+    return new Component(el);
   }
-  static createLabel(content){
-    const el = document.createElement('label')
-    el.innerHTML = content
-    return new Component(el)
+  static createLabel(content) {
+    const el = document.createElement("label");
+    el.innerHTML = content;
+    return new Component(el);
   }
 }
 
-
 class ComponentUtil {
   static getRadioChoiceElement(name, value, label) {
-    return CF.create('div')
-    .child(
-      CF.createInput(name,'radio', value, 'radio-btn').get(),
-      CF.createLabel(label).get()
-    ).get()
+    return CF.create("div")
+      .child(
+        CF.createInput(name, "radio", value, "radio-btn").get(),
+        CF.createLabel(label).get()
+      )
+      .get();
   }
 }
 
@@ -97,40 +97,29 @@ class CourseLink extends HTMLElement {
 }
 
 class ExcerciseQuiz extends HTMLElement {
-  static observedAttributes = ["question", "choices", "correct_choice", "choices_reason"];
+  static observedAttributes = [
+    "exercise_name",
+    "exercise_label",
+    "choices",
+    "selected_choice",
+    "correct_choice",
+    "choices_reason",
+  ];
 
+  // attribute
+  exercise_name = "";
+  exercise_label = "default question";
+
+  // specifict attribute
   choices = [];
-  question = "default question"
-  correct_choice = 1
-  message = "none"
-  choices_reason = []
+  selected_choice = 0;
+  correct_choice = 1;
+  choices_reason = [];
 
   submitQuestion = (e) => {
-    e.preventDefault()
-    let valueIndex = 0
-    this.shadowRoot.querySelectorAll('input').forEach((el, i) => {
-      if(el.checked){
-        valueIndex = i
-      }
-    })
-    const messageSuccessEl = this.shadowRoot.querySelector('.message-success')
-    const messageErrorEl = this.shadowRoot.querySelector('.message-err')
-    if(valueIndex == this.correct_choice-1){
-      messageSuccessEl.style.visibility = "visible"
-      messageErrorEl.style.visibility = "hidden"
-    }else{
-      messageSuccessEl.style.visibility = "hidden"
-      messageErrorEl.style.visibility = "visible"
-    }
-    let reasonContainer = this.shadowRoot.querySelector('.message-reason')
-      console.log(reasonContainer)
-    if(this.choices_reason.length>0){
-      let reasonContainer = this.shadowRoot.querySelector('.message-reason')
-      reasonContainer.innerHTML = ""
-      reasonContainer.appendChild(CF.create('div').class('reason-text').content(this.choices_reason[valueIndex]).get())
-    }
-  }
-
+    e.preventDefault();
+    this.render();
+  };
 
   constructor() {
     // Always call super first in constructor
@@ -139,39 +128,81 @@ class ExcerciseQuiz extends HTMLElement {
     let templateContent = template.content;
 
     const shadowRoot = this.attachShadow({ mode: "open" });
-    this.style.display = "block"
+    this.style.display = "block";
     shadowRoot.appendChild(templateContent.cloneNode(true));
-    let el = this.shadowRoot.querySelector("form")
-    el.addEventListener('submit',this.submitQuestion)
+    let el = this.shadowRoot.querySelector("form");
+    el.addEventListener("submit", this.submitQuestion);
   }
 
   attributeChangedCallback(name, oldValue, newValue) {
-    console.log(name)
+    console.log(name);
     if (name == "choices") {
       this.choices = newValue.split(",");
     }
-    if (name == "question"){
-      this.question = newValue
+    if (name == "exercise_label") {
+      this.exercise_label = newValue;
     }
-    if (name == "correct_choice"){
-      this.correct_choice = parseInt(newValue)
+    if (name == "selected_choice") {
+      this.selected_choice = parseInt(newValue);
     }
-    if (name == "choices_reason"){
-      this.choices_reason = newValue.split(",")
+    if (name == "correct_choice") {
+      this.correct_choice = parseInt(newValue);
     }
+    if (name == "choices_reason") {
+      this.choices_reason = newValue.split(",");
+    }
+    this.initialRender();
+  }
+
+  initialRender() {
+    let questionLabel = this.shadowRoot.querySelector(".legend");
+    questionLabel.innerHTML = this.exercise_label;
+    let container = this.shadowRoot.querySelector(".container");
+    container.innerHTML = "";
+    console.log(this.choices);
+    this.choices.forEach((choice, i) => {
+      let choiceElement = ComponentUtil.getRadioChoiceElement(
+        `${this.exercise_name}_choice`,
+        choice,
+        choice
+      );
+      choiceElement.addEventListener("click", (e) => {
+        this.selected_choice = i + 1;
+        this.shadowRoot.querySelectorAll("input").forEach((el, i) => {
+          if (i + 1 == this.selected_choice) {
+            el.checked = true;
+          } else {
+            el.checked = false;
+          }
+        });
+      });
+      container.appendChild(choiceElement);
+    });
+
     this.render();
   }
 
   render() {
-    let questionLabel = this.shadowRoot.querySelector(".legend")
-    questionLabel.innerHTML = this.question
-    let container = this.shadowRoot.querySelector(".container");
-    container.innerHTML = ""
-    this.choices.forEach((choice) => {
-      container.appendChild(
-        ComponentUtil.getRadioChoiceElement("choice", choice, choice)
+    if (this.selected_choice == 0) return;
+    const messageSuccessEl = this.shadowRoot.querySelector(".message-success");
+    const messageErrorEl = this.shadowRoot.querySelector(".message-err");
+    if (this.selected_choice == this.correct_choice) {
+      messageSuccessEl.style.visibility = "visible";
+      messageErrorEl.style.visibility = "hidden";
+    } else {
+      messageSuccessEl.style.visibility = "hidden";
+      messageErrorEl.style.visibility = "visible";
+    }
+    if (this.choices_reason.length > 0) {
+      let reasonContainer = this.shadowRoot.querySelector(".message-reason");
+      reasonContainer.innerHTML = "";
+      reasonContainer.appendChild(
+        CF.create("div")
+          .class("reason-text")
+          .content(this.choices_reason[valueIndex])
+          .get()
       );
-    });
+    }
   }
 }
 
